77444c579ed07919c050094552a71a503483bfcc
Brian OLeary <oleary.brian1@gmail.com>
2020-08-18 22:29:09 -0400

    done for the night

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 // array of questions for user
 const questions = [
-
+// What is tha name of the app?
+    {
+        type: 'input',
+        name: 'repositoryName',
+        message: 'What is the name of your application?',
+    },
+    {
+        type: 'input',
+        name: 'gitHubUserName',
+        message: 'What is your git hub user name?',
+    }
 ];
 
 // function to write README file
@@ -9,8 +19,24 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
+    console.log("File is working")
+    inquirer.prompt(questions).then((response) => {
 
+        // this will Create the first line and main header for the repo (that is what the "# " is for) using the users resonse to RepoName
+        fs.appendFileSync("README.md", ("# " + response.repositoryName) + '\n', function (err) {
+
+            if (err) {
+                console.log(err)
+            }
+            else {
+                console.log("Success")
+            }
+
+        })
+
+    })
 }
 
 // function call to initialize program
 init();
+
